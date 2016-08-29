1dd6db642dbffe308a65710028ec426d627cc88f
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-08-29 18:42:23 -0400

    Added models

--- slack_app/models.py
+++ slack_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=5000)
+    tag = models.CharField(max_length=100)
+    comment = models.CharField(max_length=100)
+    user = models.ForeignKey(User)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    question_text = models.CharField(max_length=5000)
+    score = models.IntegerField()
+    comment = models.CharField(max_length=100)
+    user = models.ForeignKey(User)
+
+class User(models.Model):
+    score = models.IntegerField()
+
+
+
+
+
+#
+# class Board(models.Model):
+#     name = models.CharField(max_length=50)
+#     users = models.ManyToManyField(User)
+#
+#
+# class Status(models.Model):
+#     name = models.CharField(max_length=20)
+#     order = models.IntegerField()
+#
+#
+# class Task(models.Model):
+#     user_story = models.TextField()
+#     weight = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(21)])
+#     board = models.ForeignKey(Board)
+#     status = models.ForeignKey(Status)
